a7a00c13d88811c1d3b2ec3450c1ed279eec339a
michael dimanshtein <michaeldimanshtein@gmail.com>
2017-08-14 18:19:08 -0500

    Changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var express = require("express");
 var app = express();
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
